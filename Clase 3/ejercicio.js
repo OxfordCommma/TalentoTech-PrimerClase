7783fdb436a2e562916a95aeb36d69d84879a78c
Ramiro Farias <ramirofarias4@gmail.com>
2026-09-13 17:06:00 -0300

    Clase 3 commit

---
+++ Clase 3/ejercicio.js
@@ -0,0 +1,22 @@
+/* “Imaginá que los precios de los productos del cliente están listos para 
+cargarse en el sistema”. Pero hay un detalle: antes de enviarlos, 
+tenés que calcular el IVA del 21% y presentarlos de manera clara.  */
+ 
+// 1.  Creá un array con 10 números que representen los precios de los productos.
+
+let prices = [50, 100, 125, 150, 175, 200, 225, 250, 275, 300];
+
+// 2.  Utiliza un método de array para calcular el precio con IVA incluido para cada valor.
+
+
+ 
+Ejercicio 2 
+Ahora que tenés los valores con IVA calculados, es momento de mostrarlos 
+como un desarrollador profesional. Matías te desafía a usar template literals 
+para estructurar tu salida de esta forma: 
+El precio es: ${valor}.- IVA incluido. 
+Imprimí cada precio ajustado en la consola siguiendo este formato. Cada mensaje debe ser 
+claro y profesional, como si estuvieras preparando un reporte para el cliente. 
+ ¿Puedes demostrar tu capacidad para manejar datos y presentar resultados de manera 
+impecable?  
+¡Este es tu momento! 
--- Clase 3/functions.js
+++ Clase 3/functions.js
@@ -17,20 +17,21 @@ console.log(suma(5, 3)); // Output: 8
 console.log(resta(5, 3)); // Output: 2
 console.log(multiplicacion(5, 3)); // Output: 15
 
-//Diferencia entre funciones y metodos
-//Una funcion es un bloque de codigo que realiza una tarea especifica y puede ser reutilizada en diferentes partes del programa. 
-//Un metodo es una funcion que esta asociada a un objeto y se utiliza para manipular o acceder a los datos de ese objeto.
+// Diferencias
+// Función: bloque de codigo que realiza una tarea especifica y puede reutilizarse en diferentes partes del programa. 
+// Método: funcion asociada a un objeto y utilizada para manipular o acceder a los datos de ese objeto.
 
-//Diferencia entre parametros y argumentos
-//Los parametros son variables que se definen en la declaracion de una funcion y se utilizan para recibir valores cuando la funcion es llamada.
-//Los argumentos son los valores que se pasan a una funcion cuando se llama, y se asignan a los parametros correspondientes.
+//Diferencias
+// Parámetros: variables definidas en la declaracion de una funcion y se utilizan para recibir valores cuando la funcion es llamada.
+// Argumentos: valores que se pasan a una funcion cuando se llama, y se asignan a los parametros correspondientes.
 
+//---------------------------------------
+
+//HOF (High Order Function):  funcion que recibe otra funcion como argumento o devuelve una funcion como resultado.
 function calculadora(a, b, operacion) {
     return operacion(a, b);
 }
 
-//HOF: High Order Function, es una funcion que recibe otra funcion como argumento o devuelve una funcion como resultado.
-
-console.log(calculadora(5, 3, suma)); // Output: 8
-console.log(calculadora(5, 3, resta)); // Output: 2
-console.log(calculadora(5, 3, multiplicacion)); // Output: 15
+console.log(calculadora(5, 3, suma));
+console.log(calculadora(5, 3, resta));
+console.log(calculadora(5, 3, multiplicacion));
